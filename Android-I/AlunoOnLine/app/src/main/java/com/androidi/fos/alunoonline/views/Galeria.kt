d571d1cc2cbd2a9c994b7036bd99a31b98afa1ed
Fábio Sales <fabio.oliveira.sales@gmail.com>
2018-05-14 22:58:38 -0300

    no message

--- Android-I/AlunoOnLine/app/src/main/java/com/androidi/fos/alunoonline/views/Galeria.kt
+++ Android-I/AlunoOnLine/app/src/main/java/com/androidi/fos/alunoonline/views/Galeria.kt
@@ -11,33 +11,36 @@ class Galeria @JvmOverloads constructor(
         attrs: AttributeSet? = null,
         defStyleAttr: Int = 0) : View(context, attrs, defStyleAttr) {
 
-    val tempoCarrossel: Long = 4000
+    val TOTAL_IMAGEM = 2
+    val tempoCarrossel: Long = 3000
     var imagens = intArrayOf(R.drawable.img0, R.drawable.img1, R.drawable.img2)
-    var contador: Int = 0
+    var contador: Int? = null
+
+    private fun imagem(): Bitmap =
+            BitmapFactory.decodeResource(resources, imagens[getContador()])
+
+    private fun getContador(): Int =
+            if (contador == null || contador == TOTAL_IMAGEM.toInt()) {
+                contador = 0
+                contador!!
+            } else {
+                contador = contador!! + 1
+                contador!!
+            }
+
+    private fun getXBitmap(bmp: Bitmap): Float = width.toFloat() / bmp.width.toFloat()
+
+    private fun getYBitmap(bmp: Bitmap): Float = height.toFloat() / bmp.height.toFloat()
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        var paint = Paint()
-        val bmp = BitmapFactory.decodeResource(resources, imagens[contador])
+        val bmp = imagem()
         val matrix = Matrix()
-
-        var escalaX: Float = width.toFloat() / bmp.width.toFloat()
-        var escalaY: Float = height.toFloat() / bmp.height.toFloat()
-
-        matrix.postScale(escalaX, escalaY)
-        val dstbmp = Bitmap.createBitmap(bmp, 0, 0, bmp.width, bmp.height, matrix, true)
+        matrix.postScale(getXBitmap(bmp), getYBitmap(bmp))
         canvas.drawColor(Color.TRANSPARENT)
-
-        canvas.drawBitmap(dstbmp, 0f, 0f, paint)
-
+        canvas.drawBitmap(Bitmap.createBitmap(bmp, 0, 0, bmp.width, bmp.height, matrix, true), 0f, 0f, Paint())
         postInvalidateDelayed(tempoCarrossel)
-
-        if (contador == 2) {
-            contador = 0
-        } else {
-            contador += 1
-        }
     }
 
 }
