c7ecc19201f712abd10ea730eb78d0200a11cad3
Fábio de Oliveira Sales <fabio.oliveira.sales@gmail.com>
2018-05-23 21:54:47 -0300

    no message

--- Android-I/AlunoOnLine/app/src/main/java/com/androidi/fos/alunoonline/activity/Usuario.kt
+++ Android-I/AlunoOnLine/app/src/main/java/com/androidi/fos/alunoonline/activity/Usuario.kt
@@ -1,7 +1,7 @@
 package com.androidi.fos.alunoonline.activity
 
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
 import com.androidi.fos.alunoonline.R
 import kotlinx.android.synthetic.main.toolbar.*
 
@@ -10,6 +10,7 @@ class Usuario : AlunoOnLineBaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_usuario)
+
         setSupportActionBar(toolbar)
         getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
         getSupportActionBar()?.setDisplayShowHomeEnabled(true)
