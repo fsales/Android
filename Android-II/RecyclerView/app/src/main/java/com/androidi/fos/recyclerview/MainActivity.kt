07f7dcc922ae08fa16980565a030064c4bd08817
Fábio Sales <fabio.oliveira.sales@gmail.com>
2018-06-19 17:42:52 -0300

    no message

--- Android-II/RecyclerView/app/src/main/java/com/androidi/fos/recyclerview/MainActivity.kt
+++ Android-II/RecyclerView/app/src/main/java/com/androidi/fos/recyclerview/MainActivity.kt
@@ -1,12 +1,62 @@
 package com.androidi.fos.recyclerview
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var dataSource: List<Usuario>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dataSource = listOf<Usuario>(
+                Usuario(nome = "Camilo"),
+                Usuario(nome = "Matheus"),
+                Usuario(nome = "Arthur"))
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = AdaptadorUsuario(dataSource)
+        recyclerView.adapter.notifyDataSetChanged()
+
+    }
+
+    inner class AdaptadorUsuario(private val dataSource: List<Usuario>) : RecyclerView.Adapter<UsuarioViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsuarioViewHolder {
+            val v = LayoutInflater.from(parent.context).inflate(R.layout.usuario_item_lista, parent, false)
+
+            return UsuarioViewHolder(v)
+        }
+
+        override fun getItemCount(): Int {
+            return dataSource.size
+        }
+
+        override fun onBindViewHolder(holder: UsuarioViewHolder, position: Int) {
+            var dado = dataSource[position]
+            holder.nomeUsuario.text = dado.nome
+        }
+
+
+    }
+
+    inner class UsuarioViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var nomeUsuario: TextView
+
+        init {
+            nomeUsuario = itemView.findViewById(R.id.nomeUsuarioTextView)
+        }
     }
 }
+
+
+class Usuario(var nome: String? = "")
