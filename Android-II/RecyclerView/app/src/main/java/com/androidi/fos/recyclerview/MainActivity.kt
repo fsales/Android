d4b7c9159c13f6406cfb809c25bbbfdc7e5b1e48
Fábio Sales <fabio.oliveira.sales@gmail.com>
2018-06-26 19:40:08 -0300

    no message

--- Android-II/RecyclerView/app/src/main/java/com/androidi/fos/recyclerview/MainActivity.kt
+++ Android-II/RecyclerView/app/src/main/java/com/androidi/fos/recyclerview/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.androidi.fos.recyclerview
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
@@ -7,6 +8,9 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
+import android.widget.SearchView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.toolbar.*
@@ -14,41 +18,128 @@ import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var dataSource: List<Usuario>
+    var dataSource: MutableList<Usuario> = mutableListOf<Usuario>()
+    lateinit var adptador: AdaptadorUsuario
+
+    val dadosRecyclerView = mutableListOf<Usuario>(
+            Usuario(nome = "Camilo"),
+            Usuario(nome = "Matheus"),
+            Usuario(nome = "Arthur"))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        dataSource = listOf<Usuario>(
-                Usuario(nome = "Camilo"),
-                Usuario(nome = "Matheus"),
-                Usuario(nome = "Arthur"))
-
         recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = AdaptadorUsuario(dataSource)
-        recyclerView.adapter.notifyDataSetChanged()
+        adptador = AdaptadorUsuario(dataSource, this)
+        recyclerView.adapter = adptador
+        //  recyclerView.adapter.notifyDataSetChanged()
+
+        editsearch.queryHint = "Teste"
+
+        editsearch.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(query: String?): Boolean {
+                adptador.filter.filter(query)
+                return false
+            }
+
+        })
+
+        dataSource.addAll(dadosRecyclerView)
 
     }
 
-    inner class AdaptadorUsuario(private val dataSource: List<Usuario>) : RecyclerView.Adapter<UsuarioViewHolder>() {
+
+    inner class AdaptadorUsuario(private var lista: List<Usuario>, private var context: Context) : RecyclerView.Adapter<UsuarioViewHolder>(), Filterable {
+        internal var filterList: List<Usuario>
+
+        init {
+            filterList = lista
+        }
+
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsuarioViewHolder {
-            val v = LayoutInflater.from(parent.context).inflate(R.layout.usuario_item_lista, parent, false)
+            val v = LayoutInflater.from(context).inflate(R.layout.usuario_item_lista, parent, false)
 
             return UsuarioViewHolder(v)
         }
 
         override fun getItemCount(): Int {
-            return dataSource.size
+            return lista.size
         }
 
         override fun onBindViewHolder(holder: UsuarioViewHolder, position: Int) {
-            var dado = dataSource[position]
+            var dado = lista[position]
             holder.nomeUsuario.text = dado.nome
         }
 
+        // filtro de usuário
+        override fun getFilter(): Filter {
+            return CustomFilter(this, filterList)
+        }
 
+        inner class CustomFilter(private val adaptador: AdaptadorUsuario, private val lista: List<Usuario>) : Filter() {
+            override fun performFiltering(query: CharSequence?): FilterResults {
+                var result = Filter.FilterResults()
+                val newLista = mutableListOf<Usuario>()
+
+                if (query?.isNotEmpty()!!) {
+                    lista.forEach { usu ->
+                        if (usu.nome?.contains(query!!, true)!!) {
+                            newLista.add(usu)
+                        }
+                    }
+
+                }
+
+                if (newLista.size > 0) {
+                    result.count = newLista.size
+                    result.values = newLista
+                } else {
+                    result.count = lista.size
+                    result.values = lista
+                }
+
+                return result
+            }
+
+            override fun publishResults(char: CharSequence?, results: FilterResults?) {
+                results?.let { r ->
+                    var lista = r.values  as? List<Usuario>
+                    lista?.let { l ->
+                        adaptador.lista = l
+                    }
+                }
+                adaptador.notifyDataSetChanged()
+            }
+
+            private fun pesquisar(array: List<String>, chaveBusca: String) {
+                val a = array.sorted()
+                val indice = pesquisaBinaria(a, 0, a.size - 1, chaveBusca)
+                if (indice >= 0) println(">>>>>>> ${a[indice]}") else print("não encontrado")
+            }
+
+
+            private fun pesquisaBinaria(array: List<String>, elementoMenor: Int, elementoMaior: Int, chaveBusca: String): Int {
+                val media = (elementoMaior + elementoMenor) / 2
+                val valorMeio = array.get(media)
+
+                return if (elementoMenor > elementoMaior) {
+                    -1
+                } else if (valorMeio.equals(chaveBusca)) {
+                    media
+                } else if (valorMeio.compareTo(chaveBusca) <0) {
+                    pesquisaBinaria(array, media + 1, elementoMaior, chaveBusca)
+                } else {
+                    pesquisaBinaria(array, elementoMenor, media - 1, chaveBusca)
+                }
+            }
+
+        }
     }
 
     inner class UsuarioViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
